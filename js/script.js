db86a2f7d7ba9d5a2c5f29bea3dde23ee29329c5
lunievas <luisa.nievas94@gmail.com>
2020-07-22 18:42:24 -0300

    mapeando os elementos com o DOM, implementando a função fetchCountries com async/await, transformando o vetor e obtendo o id, name, population e flag e implementando a função render

--- js/script.js
+++ js/script.js
@@ -1,1 +1,60 @@
-window.addEventListener('load')
+/**estado da aplicação (state) */
+
+
+//CRIANDO VARIAVEIS DE ESTADO DA APLICAÇÃO//
+let tabCountries = null;
+let tabFavorites = null;
+
+let allCountries = [];
+let favoritesCountries = [];
+
+let countCountries = 0;
+let countFavorites = 0;
+
+let totalPopulationList = 0;
+let totalPopulationFavorites = 0;
+
+let numberFormat = null;
+
+//BUSCANDO OS DADOS COM DOM, IMPLEMENTANDO A FUNC START//
+window.addEventListener('load', () =>{
+    tabCountries = document.querySelector('#tabCountries')
+    tabFavorites = document.querySelector('#tabCountries')
+    countCountries = document.querySelector('#countCountries')
+    countFavorites = document.querySelector('#countCountries')
+
+    totalPopulationList = document.querySelector('#totalPopulationList')
+    totalPopulationFavorites = document.querySelector('#totalPopulationFavorites')
+    
+    numberFormat = Intl.NumberFormat('pt-BR');
+
+    fetchCountries();
+    
+})
+
+
+//FUNÇÃO COM ASYNC/AWAIT E TRANSFORMANDO PARA PEGAR ID, NAME, POPULATION E FLAG//
+async function fetchCountries(){
+    const res = await fetch('https://restcountries.eu/rest/v2/all');
+    const json = await res.json();
+    allCountries = json.map(country =>{
+        const {numericCode,translations, population, flag} =  country;
+
+
+
+
+        return {
+            id: numericCode,
+            name: translations.pt,
+            population,
+            flag
+        };
+    });
+        render(); 
+}
+
+
+//CRIANDO A FUNÇÃO RENDER//
+function render(){
+
+}
